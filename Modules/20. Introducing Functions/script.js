39d33cb23df8f11dbe025439cc272727a95d8787
Hong-Hanh Chu <67581292+Hanmuni@users.noreply.github.com>
2025-12-10 17:36:31 +0100

    add module 20 learnings

---
+++ Modules/20. Introducing Functions/script.js
@@ -0,0 +1,10 @@
+// Defining a function
+// Running a function
+// Arguments/Paramter
+// Return Keyword
+
+function addition(number1, number2) {
+  return number1 + number2;
+}
+
+console.log(addition(2, 2));
